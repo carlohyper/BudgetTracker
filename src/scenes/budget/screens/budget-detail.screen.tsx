3dc7e8871eb5fff8c0d466621a7064dc9f961e4b
Carlo Tumala <carlo@hyperstacksinc.com>
2024-02-22 16:03:37 +0800

    fix: resolve rebase conflict

---
+++ src/scenes/budget/screens/budget-detail.screen.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+export default () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Budget</Text>
+    </View>
+  );
+};
