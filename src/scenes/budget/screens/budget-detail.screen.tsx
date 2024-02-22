9d15f74d8d8a381573ef4f6f22b93406e6da1f33
Carlo Tumala <carlo@hyperstacksinc.com>
2024-02-22 16:09:22 +0800

    fix: resolve rebase conflict

--- src/scenes/budget/screens/budget-detail.screen.tsx
+++ src/scenes/budget/screens/budget-detail.screen.tsx
@@ -12,8 +12,8 @@ function HomeScreen() {
 
 export default () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Budget</Text>
+    <View>
+      <Text>Budget Screen</Text>
     </View>
   );
 };
