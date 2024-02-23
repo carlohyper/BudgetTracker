cc8a140ce3fa278e0bef2b636bafa9e27c2d7e63
Raul Adique II <rauladiqueii@Rauls-Mac-mini.lan>
2024-02-23 15:40:17 +0800

    feat: init

--- src/scenes/expense/hooks/use-add-expense.hook.ts
+++ src/scenes/expense/hooks/use-add-expense.hook.ts
@@ -1,11 +1,26 @@
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import React from 'react';
 
 export const useAddExpense = () => {
+    const [selectedType, setSelectedType] = React.useState(0);
+
+  const expenseType = [
+    {
+      name: "Expense",
+      onPress: () => console.log("first"),
+    },
+    {
+      name: "Income",
+      onPress: () => console.log("second"),
+    },
+  ];
     const navigation = useNavigation<NativeStackNavigationProp<any>>()
 
     return {
-        
-        navigation
+        expenseType,
+        navigation,
+        selectedType,
+        setSelectedType
     }
 }
