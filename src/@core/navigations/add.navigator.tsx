1ec63a6b50ce72464997242fab0294839d403a49
Raul Adique II <rauladiqueii@Rauls-Mac-mini.lan>
2024-02-23 10:10:30 +0800

    feat: add new screen

--- src/@core/navigations/add.navigator.tsx
+++ src/@core/navigations/add.navigator.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { AddStackParamList } from '@types';
 import { AddOptionScreen } from '@scenes/add-option';
-import AddNewBudget from '@scenes/budget/screens/add-new-budget.screen';
+import { 
+  AddNewBudgetScreen,
+  NewBudgetItemsScreen
+} from '@scenes/budget';
 
 const Stack = createNativeStackNavigator<AddStackParamList>();
 
@@ -13,9 +15,10 @@ export function AddNavigator() {
   const initialRouteName: keyof AddStackParamList = "Add Option"
 
   return (
-    <Stack.Navigator initialRouteName={initialRouteName}>
+    <Stack.Navigator initialRouteName={initialRouteName} screenOptions={{ headerTitleAlign: "center"}}>
       <Stack.Screen name="Add Option" component={AddOptionScreen} />
-      <Stack.Screen name="New Budget" component={AddNewBudget} />
+      <Stack.Screen name="New Budget" component={AddNewBudgetScreen} />
+      <Stack.Screen name="New Budget Items" component={NewBudgetItemsScreen} />
     </Stack.Navigator>
   )
 }
