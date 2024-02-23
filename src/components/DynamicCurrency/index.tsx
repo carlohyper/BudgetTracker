e42917f23d61f35cc243cb2ae2b63988460fb6fe
Carlo Tumala <carlo@hyperstacksinc.com>
2024-02-23 11:33:35 +0800

    feat: add DynamicCurrency and improve UI

---
+++ src/components/DynamicCurrency/index.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { Alert, StyleSheet, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+interface CurrencyProps {
+  icon: any;
+  amount: string;
+  size: number;
+  color: string;
+  weight: any;
+}
+
+export const styles = StyleSheet.create({
+  icon: {
+    textAlign: "center",
+    borderRadius: 10,
+    marginRight: 10,
+  },
+});
+
+export const DynamicCurrency: React.FC<Partial<CurrencyProps>> = ({
+  icon = "currency-php",
+  amount = 0,
+  size = 15,
+  color = "#000000",
+  weight = "normal",
+}) => {
+  return (
+    <Text style={{ fontSize: size, fontWeight: weight, color }}>
+      <MaterialCommunityIcons name={icon} style={styles.icon} size={size} />
+      {amount}
+    </Text>
+  );
+};
--- src/scenes/budget/components/budget-plan.tsx
+++ src/scenes/budget/components/budget-plan.tsx
@@ -1,3 +1,45 @@
-import * as React from 'react';
+import * as React from "react";
+import { Alert, Text } from "react-native";
 
-export const BudgetPlan = () => {}
+import { ActionContainer, styles } from "../styles/budget-details.style";
+
+import { DynamicCard, DynamicCurrency } from "@components";
+
+interface BudgetPlanProps {
+  data: string;
+}
+
+export const BudgetPlan: React.FC<Partial<BudgetPlanProps>> = ({ data }) => {
+  return (
+    <ActionContainer>
+      <DynamicCard
+        title="Income"
+        showActionIcon={true}
+        onPress={() => Alert.alert("Add Income")}
+      >
+        <Text>List of Income Here</Text>
+      </DynamicCard>
+      <DynamicCard
+        title="Utilities"
+        onPress={() => Alert.alert("Add Utilities")}
+        subLabel={<DynamicCurrency amount="3000" />}
+      >
+        <Text>List of Utilities Here</Text>
+      </DynamicCard>
+      <DynamicCard
+        title="Savings"
+        onPress={() => Alert.alert("Add Savings")}
+        subLabel={<DynamicCurrency amount="3000" />}
+      >
+        <Text>List of Savings Here</Text>
+      </DynamicCard>
+      <DynamicCard
+        title="Miscelleneous"
+        onPress={() => Alert.alert("Add Miscelleneous")}
+        subLabel={<DynamicCurrency amount="3000" />}
+      >
+        <Text>List of Miscelleneous Here</Text>
+      </DynamicCard>
+    </ActionContainer>
+  );
+};
