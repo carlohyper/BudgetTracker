69e2e99493119c7869be040d003bde806488f3cf
BenzTan <benzwaltertan@gmail.com>
2024-02-26 10:25:51 +0800

    feat: categories scene route

--- src/@core/navigations/add.navigator.tsx
+++ src/@core/navigations/add.navigator.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { AddStackParamList } from '@types';
-import { AddOptionScreen } from '@scenes/add-option';
-import { 
-  AddNewBudgetScreen,
-  NewBudgetItemsScreen
-} from '@scenes/budget';
-import { AddExpenseScreen } from '@scenes/expense';
+import { AddStackParamList } from "@types";
+import { AddOptionScreen } from "@scenes/add-option";
+import { AddNewBudgetScreen, NewBudgetItemsScreen } from "@scenes/budget";
+import { AddExpenseScreen, CategoriesScreen } from "@scenes/expense";
 
 const Stack = createNativeStackNavigator<AddStackParamList>();
 
 export function AddNavigator() {
-
-  const initialRouteName: keyof AddStackParamList = "Add Option"
+  const initialRouteName: keyof AddStackParamList = "Add Option";
 
   return (
-    <Stack.Navigator initialRouteName={initialRouteName} screenOptions={{ headerTitleAlign: "center"}}>
+    <Stack.Navigator
+      initialRouteName={initialRouteName}
+      screenOptions={{ headerTitleAlign: "center" }}
+    >
       <Stack.Screen name="Add Option" component={AddOptionScreen} />
       <Stack.Screen name="New Budget" component={AddNewBudgetScreen} />
       <Stack.Screen name="New Budget Items" component={NewBudgetItemsScreen} />
       <Stack.Screen name="Add Expense" component={AddExpenseScreen} />
-
+      <Stack.Screen name="Categories" component={CategoriesScreen} />
     </Stack.Navigator>
-  )
+  );
 }
