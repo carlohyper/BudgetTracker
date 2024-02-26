47d020dcbf726da7dbdf8db212563e31661daa84
Raul Adique II <rauladiqueii@Rauls-Mac-mini.lan>
2024-02-26 15:56:38 +0800

    refactor: remove white space

--- src/scenes/expense/components/expense-type.tsx
+++ src/scenes/expense/components/expense-type.tsx
@@ -14,6 +14,7 @@ export interface ExpenseType {
 
 export default ({ showModal, selectedCategory }: ExpenseType) => {
   const { expenseType, selectedType, setSelectedType } = useAddExpense();
+
   return (
     <>
       <ExpenseTypeContainer>
@@ -29,6 +30,7 @@ export default ({ showModal, selectedCategory }: ExpenseType) => {
           </ExpenseTypeItem>
         ))}
       </ExpenseTypeContainer>
+
       {selectedType === 0 ? (
         <ExpenseForm
           showModal={showModal}
